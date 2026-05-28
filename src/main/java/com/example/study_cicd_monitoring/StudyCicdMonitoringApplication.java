c38fb2fd13373ca0406a369807477740170d9d52
shinukjin <swj0507@gmail.com>
2026-05-28 11:56:27 +0900

    init

---
+++ src/main/java/com/example/study_cicd_monitoring/StudyCicdMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_cicd_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCicdMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyCicdMonitoringApplication.class, args);
+	}
+
+}
